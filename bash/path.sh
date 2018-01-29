d42ee4abd8d488875a4635a3d3834289e87e86c0
Chris Davis <defbyte@gmail.com>
2018-01-29 16:08:01 -0500

    Remove HEROKU_PATH
    Because we will install via homebrew now

--- bash/path.sh
+++ bash/path.sh
@@ -1,12 +1,11 @@
 # Custom $PATH
 
 export USR_PATH="/usr/local/bin:/usr/local/sbin"
-export HEROKU_PATH="/usr/local/heroku/bin"
-export PG_PATH="/Applications/Postgres.app/Contents/MacOS/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/Applications/Postgres.app/Contents/Versions/9.6/bin"
+export PG_PATH="/Applications/Postgres.app/Contents/Versions/latest/bin"
 export NODE_PATH="/usr/local/share/npm/bin"
 export OSX_PATH="/usr/bin:/usr/sbin:/bin:/sbin"
 
-export PATH="$USR_PATH:$HEROKU_PATH:$PG_PATH:$NODE_PATH:$OSX_PATH"
+export PATH="$USR_PATH:$PG_PATH:$NODE_PATH:$OSX_PATH"
 
 # Activate rbenv
 export RBENV_ROOT="/usr/local/var/rbenv"
